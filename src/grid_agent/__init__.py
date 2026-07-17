4d45d68889dc2c2ed8d1af69af57e9819697b6fd
paperplane123 <1697620694@qq.com>
2026-07-17 10:47:15 +0800

    feat: export GridSociety simulation API

--- src/grid_agent/__init__.py
+++ src/grid_agent/__init__.py
@@ -2,16 +2,34 @@
 
 from .diagnosis import FaultDiagnosis, FaultDiagnoser
 from .model import Branch, FeederCase, Measurement, Node
+from .society import (
+    DiagnosticAssessment,
+    GridSocietySimulator,
+    IncidentConfig,
+    RuleBasedDispatcher,
+    SimulationReport,
+    SimulationScenario,
+    load_scenario,
+    run_simulation,
+)
 from .topology import RadialFeeder
 
 __all__ = [
     "Branch",
+    "DiagnosticAssessment",
     "FaultDiagnosis",
     "FaultDiagnoser",
     "FeederCase",
+    "GridSocietySimulator",
+    "IncidentConfig",
     "Measurement",
     "Node",
     "RadialFeeder",
+    "RuleBasedDispatcher",
+    "SimulationReport",
+    "SimulationScenario",
+    "load_scenario",
+    "run_simulation",
 ]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
